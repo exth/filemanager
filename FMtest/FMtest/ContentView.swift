a7ce6d62161c59440f917772bdc8203858cb1144
Вадим <exth331@gmail.com>
2026-06-01 05:03:25 +0300

    dd

---
+++ FMtest/FMtest/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  FMtest
+//
+//  Created by Вадим on 01.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("ff")
+            Text("ff")
+            Text("ff")
+            Text("ff")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FMtest/FMtest/FMtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FMtestApp.swift
+//  FMtest
+//
+//  Created by Вадим on 01.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct FMtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
